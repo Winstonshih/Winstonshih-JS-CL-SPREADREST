c4f8a34fca7a7d8823999ba841c732581ebadbfe
Winstonshih <Winstonshih@github.com>
2026-09-21 17:08:57 +0000

    Test run: 1/2 passed, 1 failed, 0 skipped - 2026-09-21T17:08:57.549Z

--- src/main/scripts.js
+++ src/main/scripts.js
@@ -17,15 +17,17 @@ let sumElement = document.getElementById("sum");
 // TODO: Use the spread/rest operator so the function below properly invokes the renderEmployee() function.
 function submitEmployee() {
     let employeeProfile = ['John', 'Smith', '89,000'];
-    renderEmployee(  );
+    renderEmployee(...employeeProfile);
 }
 
 
 // TODO: Refactor the function below to take in any amount of numbers and render the sum on the page.
-function sum(  ) {
+function sum(n) {
     let sumNumbers = 0;
-
-
+    for(let i=0;i<n;i++)
+    {
+        sumNumber+=i;
+    }    
     sumElement.innerText = sumNumbers;
 }
 
